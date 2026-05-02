7ebd472e5b2e990238a0bec4a088ec56f77af774
MarianaMayumeCaniza <caniza.mariana@unifesp.br>
2026-05-01 23:10:45 -0300

    Dia3

---
+++ Dia3/GerenciamentoMindfulness/Sources/GerenciamentoMindfulness/GerenciamentoMindfulness.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct GerenciamentoMindfulness {
+    static func main() {
+        print("Hello, world!")
+    }
+}
